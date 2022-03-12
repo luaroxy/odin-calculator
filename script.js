8f578dccde0d77e886e8f8ddffd18497727b0b15
luaroxy <luannamsiqueira@gmail.com>
2022-03-11 22:37:21 -0600

    Attached buttons to operate function

--- script.js
+++ script.js
@@ -32,11 +32,51 @@ const operate = function (operator, a, b){
 
 const buttons = document.querySelectorAll('button');
 const display = document.querySelector('#display');
+numbersToOperate=[];
+
+const displayss = function (){
+  displayValue = this.textContent;
+  display.textContent = displayValue;
+  numbersToOperate.push(displayValue);
+  if (numbersToOperate.includes("=")){
+
+    if(numbersToOperate.includes("+")){
+      operator = "+";
+      tempInd = numbersToOperate.indexOf("+");
+    }
+
+    if(numbersToOperate.includes("-")){
+      operator = "-";
+      tempInd = numbersToOperate.indexOf("-");
+    }
+
+    if(numbersToOperate.includes("*")){
+      operator = "*";
+      tempInd = numbersToOperate.indexOf("*");
+    }
+
+    if(numbersToOperate.includes("/")){
+      operator = "/";
+      tempInd = numbersToOperate.indexOf("/");
+    }
+
+    a = parseInt(numbersToOperate.slice(0,tempInd).join(""));
+    b = parseInt(numbersToOperate.slice(tempInd+1, numbersToOperate.length-1).join(""));
+    console.log(a);
+    console.log(b);
+
+    operate(operator,a,b);
+  }
+
+}
 
 buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-    displayValue = button.textContent;
-    display.textContent = displayValue;
-    });
-  
+    button.addEventListener('click', displayss);
 });
+
+//buttons.forEach((button) => {
+//  button.addEventListener('click', () => {
+//  displayValue = button.textContent;
+//  display.textContent = displayValue;
+//  });
+//});
