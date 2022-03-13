dc25c92e856da68a1cea9819bca772af059a026c
luaroxy <luannamsiqueira@gmail.com>
2022-03-12 18:14:26 -0600

    Improved display and calculations

--- script.js
+++ script.js
@@ -17,66 +17,74 @@ const divide = function(a,b) {
 const operate = function (operator, a, b){
   switch(operator){
     case "+":
-      console.log(add(a,b))
+      return add(a,b)
       break
     case "-":
-      console.log(subtract(a,b))
+      return subtract(a,b)
       break
     case "*":
-      console.log(multiply(a,b))
+      return multiply(a,b)
       break
     case "/":
-      console.log(divide(a,b))
+      return divide(a,b)
   }
+}
+
+const startCalculator = function (){
+  displayValue = this.textContent;
+  test=this.className;
+  if (test == "numbers") {
+    display.textContent += displayValue;
+  } else display.textContent = "";
+
+  displayAll.textContent += displayValue;
+
+  numbersToOperate.push(displayValue);
+  if (numbersToOperate.includes("=")){
+    calculationResult = calculate();
+    display.textContent=calculationResult;
+    displayAll.textContent = calculationResult;
+  }
+}
+
+const calculate = function(){
+  if(numbersToOperate.includes("+")){
+    operator = "+";
+    tempInd = numbersToOperate.indexOf("+");
+  }
+
+  if(numbersToOperate.includes("-")){
+    operator = "-";
+    tempInd = numbersToOperate.indexOf("-");
+  }
+
+  if(numbersToOperate.includes("*")){
+    operator = "*";
+    tempInd = numbersToOperate.indexOf("*");
+  }
+
+  if(numbersToOperate.includes("/")){
+    operator = "/";
+    tempInd = numbersToOperate.indexOf("/");
+  }
+
+  a = parseInt(numbersToOperate.slice(0,tempInd).join(""));
+  b = parseInt(numbersToOperate.slice(tempInd+1, numbersToOperate.length-1).join(""));
+  console.log(a);
+  console.log(b);
+
+  numbersToOperate = [];
+  numbersToOperate.push(operate(operator,a,b));
+
+  return numbersToOperate;
 }
 
 const buttons = document.querySelectorAll('button');
 const display = document.querySelector('#display');
+const displayAll = document.querySelector('#displayAll');
 numbersToOperate=[];
 
-const displayss = function (){
-  displayValue = this.textContent;
-  display.textContent = displayValue;
-  numbersToOperate.push(displayValue);
-  if (numbersToOperate.includes("=")){
-
-    if(numbersToOperate.includes("+")){
-      operator = "+";
-      tempInd = numbersToOperate.indexOf("+");
-    }
-
-    if(numbersToOperate.includes("-")){
-      operator = "-";
-      tempInd = numbersToOperate.indexOf("-");
-    }
-
-    if(numbersToOperate.includes("*")){
-      operator = "*";
-      tempInd = numbersToOperate.indexOf("*");
-    }
-
-    if(numbersToOperate.includes("/")){
-      operator = "/";
-      tempInd = numbersToOperate.indexOf("/");
-    }
-
-    a = parseInt(numbersToOperate.slice(0,tempInd).join(""));
-    b = parseInt(numbersToOperate.slice(tempInd+1, numbersToOperate.length-1).join(""));
-    console.log(a);
-    console.log(b);
-
-    operate(operator,a,b);
-  }
-
-}
-
 buttons.forEach((button) => {
-    button.addEventListener('click', displayss);
+    button.addEventListener('click', startCalculator);
 });
 
-//buttons.forEach((button) => {
-//  button.addEventListener('click', () => {
-//  displayValue = button.textContent;
-//  display.textContent = displayValue;
-//  });
-//});
