ee120330337fd1f91c837f824a8728d433617cc2
luaroxy <luannamsiqueira@gmail.com>
2022-03-26 23:24:32 -0600

    Done

--- script.js
+++ script.js
@@ -2,41 +2,121 @@ const buttons = document.querySelectorAll('button');
 const display = document.querySelector('#display');
 const displayAll = document.querySelector('#displayAll');
 numbersToOperate=[];
+calculationResult=[];
+checkEqual = false;
 
 const startCalculator = function (){
+  calculateBeforeEqual = false;
+  repeatOperator = false;
+
   displayValue = this.textContent;
-  console.log(this.textContent)
   checkDisplayType=this.className;
+
+  // Check if client repeated operator and only consider the most recent
+  repeatOpIndex = numbersToOperate.length-1;
+  if (checkDisplayType == "operators" && (numbersToOperate[repeatOpIndex]=="+" || numbersToOperate[repeatOpIndex]=="-" || numbersToOperate[repeatOpIndex]=="*" || numbersToOperate[repeatOpIndex]=="+")){
+    backspace();
+  }
+
+  // Check if client entered a new number after the result of a previous operation, if yes, only consider new number
+  if(checkDisplayType == "numbers" && checkEqual == true){
+    clear();
+    checkEqual = false;
+  }
+
+  //If number add continue adding value in desplay, otherwise don't add
   if (checkDisplayType == "numbers") {
     display.textContent += displayValue;
   } else display.textContent = "";
+  
+  //Don't add number in display if continuing from a previous operator (ex: 8+12+15) - the 15 won't add in the display, only in displayAll
+  if (checkDisplayType == "numbers" && calculationResult.length != 0){
+    display.textContent = "";
+  }
 
+  // Check if client enter an operator having already other operator (ex: 1+2+), if yes it should calculate (in the example it would be 1+2)
+  if (checkDisplayType == "operators" && (numbersToOperate.includes("+") || numbersToOperate.includes("-")|| numbersToOperate.includes("*") || numbersToOperate.includes("/"))){
+    calculateBeforeEqual = true;
+  }
+
+  // If delete it will ativate backspace function, if not it will include the value in displayAll and numbersToOperare
   if (this.textContent != "delete"){
     displayAll.textContent += displayValue;
     numbersToOperate.push(displayValue);
   } else {
-    temp = displayAll.textContent.slice(0, -1);
-    displayAll.textContent=temp;
-    numbersToOperate.pop();
+    backspace();
   }
 
+  // Disable . after first use
   if (this.textContent == "."){
     document.getElementById("dot").disabled = true;
   }
-  
-  if (numbersToOperate.includes("=")){
+
+  // Iniciate calculation
+  if (numbersToOperate.includes("=") || calculateBeforeEqual == true){
     calculationResult = calculate();
-    if (calculationResult != "NaN"){
-      display.textContent=calculationResult;
-      displayAll.textContent = calculationResult;
+    if (isNaN(calculationResult[0]) == false){
+      display.textContent=calculationResult[0];
       document.getElementById("dot").disabled = false;
     }
   }
-  console.log(this.textContent)
+
+  // Update displayAll and checkEqual if = was used
+  if (this.textContent == "=" && isNaN(calculationResult[0]) == false){
+    displayAll.textContent = calculationResult;
+    checkEqual = true;
+  }
+
+  // Clear
   if (this.textContent === "clear"){
     clear();
   }
 }
+
+const calculate = function(){
+  tempOperator=0;
+  tempCalculate=numbersToOperate.slice(0, -1);
+  if(tempCalculate.includes("+")){
+    operator = "+";
+    tempInd = numbersToOperate.indexOf("+");
+  } else if(tempCalculate.includes("-")){
+    operator = "-";
+    tempInd = numbersToOperate.indexOf("-");
+  } else if(tempCalculate.includes("*")){
+    operator = "*";
+    tempInd = numbersToOperate.indexOf("*");
+  } else if(tempCalculate.includes("/")){
+    operator = "/";
+    tempInd = numbersToOperate.indexOf("/");
+  } else {
+    disableButtons(); //disable buttons because it cannot calculate without operator
+    return;
+  }
+
+  // Calculate a and b based on the operator location
+  a = parseFloat(numbersToOperate.slice(0,tempInd).join(""));
+  b = parseFloat(numbersToOperate.slice(tempInd+1, numbersToOperate.length-1).join(""));
+  // If the user doesn't include b, b=a. For example 2+= is equal to 4
+  if (isNaN(b)){
+    b=a;
+  }
+
+  // Save the operator used to trigger the calculation as it will be used later. For example in 1+2-, the - will be saved
+  if(numbersToOperate[numbersToOperate.length-1] != "=") tempOperator=numbersToOperate[numbersToOperate.length-1];
+
+  numbersToOperate = [];
+  resultOfOperate = operate(operator,a,b);
+  
+  //Round the result to three decimal values and save it in numbersToOperate
+  numbersToOperate.push(roundToThree(resultOfOperate));
+
+  // Add the previously saved operator to continue the operations
+  if (tempOperator != 0) numbersToOperate.push(tempOperator);
+
+  return numbersToOperate;
+}
+
+//Small and self explanatory functions
 
 const add = function(a,b) {
 	return a+b;
@@ -75,44 +155,20 @@ const operate = function (operator, a, b){
   }
 }
 
-const calculate = function(){
-  if(numbersToOperate.includes("+")){
-    operator = "+";
-    tempInd = numbersToOperate.indexOf("+");
-  } else if(numbersToOperate.includes("-")){
-    operator = "-";
-    tempInd = numbersToOperate.indexOf("-");
-  } else if(numbersToOperate.includes("*")){
-    operator = "*";
-    tempInd = numbersToOperate.indexOf("*");
-  } else if(numbersToOperate.includes("/")){
-    operator = "/";
-    tempInd = numbersToOperate.indexOf("/");
-  } else {
-    disableButtons();
-    return;
-  }
-
-  a = parseFloat(numbersToOperate.slice(0,tempInd).join(""));
-  b = parseFloat(numbersToOperate.slice(tempInd+1, numbersToOperate.length-1).join(""));
-  if (isNaN(b)){
-    b=a;
-  }
-  numbersToOperate = [];
-  resultOfOperate = operate(operator,a,b);
-  
-  numbersToOperate.push(roundToThree(resultOfOperate));
-
-  return numbersToOperate;
-}
-
 const clear = function(){
   display.textContent = "";
   displayAll.textContent = "";
   numbersToOperate = [];
+  calculationResult=[];
   a=0;
   b=0;
   enableButtons();
+}
+
+const backspace = function(){
+  temp = displayAll.textContent.slice(0, -1);
+    displayAll.textContent=temp;
+    numbersToOperate.pop();
 }
 
 const roundToThree = function (num) {    
@@ -151,36 +207,29 @@ function enableButtons(){
 
 const keyboard = function(e) {
   e.preventDefault();
-  if (e.keyCode == 48 || e.keyCode == 96) document.getElementById("n0").click();
-  if (e.keyCode == 49 || e.keyCode == 97) document.getElementById("n1").click();
-  if (e.keyCode == 50 || e.keyCode == 98) document.getElementById("n2").click();
-  if (e.keyCode == 51 || e.keyCode == 99) document.getElementById("n3").click();
-  if (e.keyCode == 52 || e.keyCode == 100) document.getElementById("n4").click();
-  if (e.keyCode == 53 || e.keyCode == 101) document.getElementById("n5").click();
-  if (e.keyCode == 54 || e.keyCode == 102) document.getElementById("n6").click();
-  if (e.keyCode == 55 || e.keyCode == 103) document.getElementById("n7").click();
-  if ((e.keyCode == 56 && e.shiftKey == false) || e.keyCode == 104) document.getElementById("n8").click();
-  if (e.keyCode == 57 || e.keyCode == 105) document.getElementById("n9").click();
-  if (e.keyCode == 110 || e.keyCode == 190) document.getElementById("dot").click();
-  if (e.keyCode == 13 || (e.keyCode == 187 && e.shiftKey == false)) document.getElementById("equal").click();
-  if (e.keyCode == 107 || (e.keyCode == 187 && e.shiftKey == true)) document.getElementById("o+").click();
-  if (e.keyCode == 109 || e.keyCode == 189) document.getElementById("o-").click();
-  if (e.keyCode == 106 || (e.keyCode == 56 && e.shiftKey == true)) document.getElementById("o*").click();
-  if (e.keyCode == 111 || e.keyCode == 191) document.getElementById("o/").click();
-  if (e.keyCode == 8 || e.keyCode == 46) document.getElementById("delete").click();
+  if (e.key == 0) document.getElementById("n0").click();
+  if (e.key == 1) document.getElementById("n1").click();
+  if (e.key == 2) document.getElementById("n2").click();
+  if (e.key == 3) document.getElementById("n3").click();
+  if (e.key == 4) document.getElementById("n4").click();
+  if (e.key == 5) document.getElementById("n5").click();
+  if (e.key == 6) document.getElementById("n6").click();
+  if (e.key == 7) document.getElementById("n7").click();
+  if (e.key == 8) document.getElementById("n8").click();
+  if (e.key == 9) document.getElementById("n9").click();
+  if (e.key == ".") document.getElementById("dot").click();
+  if (e.key == "Enter") document.getElementById("equal").click();
+  if (e.key == "+") document.getElementById("o+").click();
+  if (e.key == "-") document.getElementById("o-").click();
+  if (e.key == "*") document.getElementById("o*").click();
+  if (e.key == "/") document.getElementById("o/").click();
+  if (e.key == "Backspace" || e.key == "Delete") document.getElementById("delete").click();
 }
 
+
+//Event Listeners: Buttons and keyboard
 buttons.forEach((button) => {
    button.addEventListener('click', startCalculator);
 });
 
-document.addEventListener("keyup", keyboard);
-
-
-
- // if (event.keyCode === 13) {
-    // Cancel the default action, if needed
-  //  event.preventDefault();
-    // Trigger the button element with a click
-  //  document.getElementById("myBtn").click();
- // }
+document.addEventListener("keydown", keyboard);
